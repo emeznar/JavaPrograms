ee0b98a7277fd89da661d22951ee1d2d38a82be3
emeznar <ericmeznarich@gmail.com>
2017-03-12 13:33:32 -0500

    Commit Java Examples

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,21 @@
+/**
+ * Created by emeznar on 3/11/17.
+ */
+public class Hello {
+    public static void main(String[] args){
+
+        System.out.println("Hello World! This is Eric's first Java program!");
+
+        int myFirstNumber = (10 + 5) + (2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myFirstNumber);
+        System.out.println("This is my first number");
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+
+
+    }
+}
---
+++ IntFloatConversions/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int myIntValue = 5/3;
+
+	float myFloatValue = 5f/3f;
+
+	double myDoubleValue = 5d/3d;
+
+        System.out.println("myInValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoublealue = " + myDoubleValue);
+
+    }
+}
